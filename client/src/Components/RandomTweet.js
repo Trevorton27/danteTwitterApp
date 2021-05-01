17cd71465053aad947b6d93e8fe7b9d225085018
iceeeberg <72841352+iceeeberg@users.noreply.github.com>
2021-05-01 18:14:32 -0500

    organizing files

---
+++ client/src/Components/RandomTweet.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const RandomTweet = () => {
+
+  return (
+    <div>
+    </div>
+  )
+}
+
+export default RandomTweet;
--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const express = require ('express');
 const app = express();
+const path = require('path')
+const axios = require('axios');
+const port = 3001;
+
+app.use('/' , express.static('public'));
 
 app.get("/", (req, res) => {
   console.log("Responding to root route");
   res.send("Hello from Root!");
 });
 
-app.listen(3001, () => {
-  console.log('server is listening!');
+app.get('/api/',(req, res) => {
+
+}
+
+app.listen(port, () => {
+  console.log('server is listening on port 3001!');
 });
