045c47135d2903fc0a424b4cbf2827f47a1b04f0
iceeeberg <72841352+iceeeberg@users.noreply.github.com>
2021-05-02 16:57:55 -0500

    adding internal API

--- server.js
+++ server.js
@@ -1,30 +1,33 @@
 const express = require ('express');
 const app = express();
-const path = require('path')
+const path = require('path');
 const axios = require('axios');
 const port = 3000;
 
-app.use('/' , express.static('public'));
+app.use('/' , express.static(path.join(__dirname, 'client')));
 
-app.get("/", (req, res) => {
-  console.log("Responding to root route");
-  res.send("Hello from Root!");
-});
+// app.get("/", (req, res) => {
+//   console.log("Responding to root route");
+//   res.send("Hello from Root!");
+// });
 
-app.get('/api/',(req, res) => {
+app.get('/api',(req, res) => {
 
-
-  axios.get('https://api.twitter.com')
+  axios({
+    method: 'get',
+    url: 'https://api.twitter.com/1.1/search/tweets.json?q',
+    data: 'json'
+  })
     .then((response) => {
-    // handle success
-    res.send(response.data)
+      res.send(response.data)
+      console.log(response.data)
     })
     .catch((err) => {
-    // handle err
-    console.log(err)
+      console.log(err)
+      res.sendStatus(500);
   })
 });
 
 app.listen(port, () => {
-  console.log('server is listening on port 3000!');
+  console.log(`server is listening on ${port}!`);
 });
